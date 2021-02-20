a6fac500c162405d05357078471ea5a4b8cffdff
Vidyashree jayaram <vidyashreejayaram@gmail.com>
2021-02-20 20:23:47 +0100

    Slider of related items section implemented.

--- index.js
+++ index.js
@@ -3,4 +3,19 @@ $(document).ready(function () {
         $(this).toggleClass("expanded");
         console.log("hi");
     });
+    new Splide(".splide", {
+      
+      perPage: 5,
+      pagination: false,
+      gap: "1rem",
+     
+      breakpoints: {
+        991: {
+          perPage: 3,
+        },
+        767: {
+          perPage: 1,
+        },
+      },
+    }).mount();
 });
