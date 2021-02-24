fa942fea8038ca123e616f045274f6658870de1f
Vidyashree jayaram <vidyashreejayaram@gmail.com>
2021-02-24 15:46:24 +0100

    Mega-menu implemented.

--- index.js
+++ index.js
@@ -18,23 +18,34 @@ $(document).ready(function () {
     },
   }).mount();
 
-  $(".btn-article-weight").click(function(){
+  $(".btn-article-weight").click(function () {
     let articleWeight = $(this).text().split(" ");
     compareValue(articleWeight[0]);
 
-    function compareValue(weight){
-       if(weight==="500")
-         changeValuesOnSelection("3,50","3,60", "500");
-       else if(weight==="300")
-         changeValuesOnSelection("2,80","2,90","300");
-       else
-         changeValuesOnSelection("2,10","2,60","250");  
+    function compareValue(weight) {
+      if (weight === "500") changeValuesOnSelection("3,50", "3,60", "500");
+      else if (weight === "300") changeValuesOnSelection("2,80", "2,90", "300");
+      else changeValuesOnSelection("2,10", "2,60", "250");
     }
 
-    function changeValuesOnSelection(discountedPrice,originalPrice,weight){
+    function changeValuesOnSelection(discountedPrice, originalPrice, weight) {
       $("#discounted-price").text(`${discountedPrice} €`);
       $("#original-price").text(`${originalPrice} €`);
       $("#weight").text(`${weight}`);
+    }
+  });
+
+  $("#mega-menu-button").click(function () {
+    let display = $("#mega-menu").css("display");
+
+    if (display === "none") {
+      $("#mega-menu").css("display", "block");
+      $("#mega-menu-button").css("color", "#f77600");
+      $("#mega-menu-button").css("background-color", "white");
+    } else {
+      $("#mega-menu").css("display", "none");
+      $("#mega-menu-button").css("color", "white");
+      $("#mega-menu-button").css("background-color", "transparent");
     }
   });
 });
